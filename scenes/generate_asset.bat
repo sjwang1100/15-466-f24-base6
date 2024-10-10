6cbb750474436de2afdd7f110a10661ae30b6ae7
sjwang1100 <shaojuwang1216@gmail.com>
2024-10-10 06:19:33 -0400

    assets

---
+++ scenes/generate_asset.bat
@@ -0,0 +1,11 @@
+:: Meshes
+"E:\SteamLibrary\steamapps\common\Blender\blender.exe" --background --python export-meshes.py -- cheese.blend:Collection ../dist/cheese.pnct
+"E:\SteamLibrary\steamapps\common\Blender\blender.exe" --background --python export-meshes.py -- lower_bun.blend:Collection ../dist/lower_bun.pnct
+"E:\SteamLibrary\steamapps\common\Blender\blender.exe" --background --python export-meshes.py -- patty.blend:Collection ../dist/patty.pnct
+"E:\SteamLibrary\steamapps\common\Blender\blender.exe" --background --python export-meshes.py -- upper_bun.blend:Collection ../dist/upper_bun.pnct
+"E:\SteamLibrary\steamapps\common\Blender\blender.exe" --background --python export-meshes.py -- veggies.blend:Collection ../dist/veggies.pnct
+"E:\SteamLibrary\steamapps\common\Blender\blender.exe" --background --python export-meshes.py -- walkmesh.blend:Collection ../dist/walkmesh.pnct
+
+
+::Walkmesh
+"E:\SteamLibrary\steamapps\common\Blender\blender.exe" --background --python export-walkmeshes.py -- walkmesh.blend:Collection ../dist/walkmesh.w
